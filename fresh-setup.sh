cbdaf5c89991c22d68d12010a3328794538b46ed
Ho Man Boa <boa.homan@gmail.com>
2023-03-20 10:11:02 -0400

    update setup

--- fresh-setup.sh
+++ fresh-setup.sh
@@ -19,8 +19,6 @@ brew install tmux
 brew install tmuxinator
 brew install reattach-to-user-namespace
 brew install bat
-brew install fzf
-
 
 # Oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -28,39 +26,37 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone git@github.com:aranair/dotfiles.git ~/dotfiles
 
-ln -s ~/dotfiles/vimrc ~/.vimrc
-ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
-cp ~/dotfiles/zshrc ~/.zshrc
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 
+# Do not change this to a symlink
+cp ~/dotfiles/.zshrc ~/.zshrc
 cp ~/dotfiles/karabiner.json ~/.config/karabiner/karabiner.json
 
-vim +PluginInstall +qall
+# vim-plug
+vim +PlugInstall +qall
 
-if [ ! -d "~/.vim/swp" ]; then
-  mkdir ~/.vim/swp
-fi
-
-if [ ! -d "~/.vim/backup" ]; then
-  mkdir ~/.vim/backup
-fi
-
-if [ ! -d "~/.vim/colors" ]; then
-  mkdir ~/.vim/colors
-fi
+mkdir -p ~/.vim/swp
+mkdir -p ~/.vim/backup
+mkdir -p ~/.vim/colors
 
 cp ~/dotfiles/Monokai.vim ~/.vim/colors/
 
 # Notes
+NOTES_VERSION=1.1.0
+TMP_PATH=/tmp/notes-dl
+curl -L https://github.com/pimterry/notes/archive/refs/tags/${NOTES_VERSION}.tar.gz -O && \
+  echo "160f7d5a46c8a8654241a532a5e14e128d6c832b547bfd38653dc78f06964c03  ${NOTES_VERSION}.tar.gz" | \
+  shasum -a 256 -c && \
+  tar xzf ${NOTES_VERSION}.tar.gz -C $TMP_PATH --strip-components=1 && rm ${NOTES_VERSION}.tar.gz
+
+cd $TMP_PATH && make USERDIR=$(eval echo ~$SUDO_USER)
+
 curl https://raw.githubusercontent.com/pimterry/notes/latest-release/notes > /usr/local/bin/notes && chmod +x /usr/local/bin/notes
 curl -L https://raw.githubusercontent.com/pimterry/notes/latest-release/_notes > /usr/local/share/zsh/site-functions/_notes
-
 
 # GIT
 # echo -e "\n\n\n" ssh-keygen -t rsa -b 4096 -C "boa.homan@gmail.com"
 # ssh-keygen -t rsa -b 4096 -C "comment" -P "examplePassphrase" -f "desired pathAndName" -q
 # eval "$(ssh-agent -s)"
 # ssh-add ~/.ssh/id_rsa
-
-# RVM
-/curl -sSL https://get.rvm.io | bash -s stable --ruby
-
